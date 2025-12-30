a9e7a3bf8fe6ad4bab06a47c6e0151acd2a1e073
Simon Alam <simonalam2003@gmail.com>
2025-12-30 03:01:46 +0100

    Initial commit

---
+++ src/test/java/se/kth/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package se.kth;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
